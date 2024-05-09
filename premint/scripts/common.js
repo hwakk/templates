29e6880790c21aee1259fe25f78e02be0b3a0bc7
hwakk <97441379+hwakk@users.noreply.github.com>
2024-05-10 02:08:36 +0900

    [feat] brand 페이지 퍼블리싱 작업 완료

--- premint/scripts/common.js
+++ premint/scripts/common.js
@@ -5,9 +5,11 @@ const scrollTop = document.querySelector('#scrollTop');
 // Change header background on scroll
 window.addEventListener('scroll', () => {
   if (window.scrollY > 100) {
+    if (header.classList.contains('scrolled')) return;
     header.classList.add('scrolled');
     logo.src = '/premint/images/logo.png';
   } else {
+    if (!header.classList.contains('scrolled')) return;
     header.classList.remove('scrolled');
     logo.src = '/premint/images/logo_white.png';
   }
