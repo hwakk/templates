e32d4fca9ecf07b6e7b96c3d6766b4fe1ce05540
hwakk <97441379+hwakk@users.noreply.github.com>
2024-05-09 23:52:49 +0900

    [feat] headr, footer 분리

---
+++ premint/scripts/common.js
@@ -0,0 +1,20 @@
+const header = document.querySelector('header');
+const logo = document.querySelector('#headerLogo');
+const scrollTop = document.querySelector('#scrollTop');
+
+// Change header background on scroll
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 100) {
+    header.classList.add('scrolled');
+    logo.attributes.src.value = '/premint/images/logo.png';
+  } else {
+    header.classList.remove('scrolled');
+    logo.attributes.src.value = '/premint/images/logo_white.png';
+  }
+});
+
+// Scroll to top
+scrollTop.addEventListener('click', (e) => {
+  e.preventDefault();
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
